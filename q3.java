f0c944f8aeb65e39fa6c34df5b8b0678b71505b0
sanjanadissa <148559682+sanjanadissa@users.noreply.github.com>
2024-09-29 19:43:06 +0530

    done

--- q3.java
+++ q3.java
@@ -1,3 +1,17 @@
+import  java.util.Scanner;
+
 public class q3 {
+
+   public static void main(String arg[]){
+
+    Scanner resulte = new Scanner(System.in);
+    System.out.println("enter lenght in centimeters");
+    int lenght = resulte.nextInt();
+    int feet = (int)lenght/2.54;
+    int inches = (int)lenght%2.54;
+    System.out.println("feet: " + feet + " inches: " + inches);
+
+
+   }
     
 }
