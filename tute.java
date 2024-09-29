dc8bb5bb3145aa1abd5d572c681a8ddb134f96b8
sanjanadissa <148559682+sanjanadissa@users.noreply.github.com>
2024-09-29 14:49:22 +0530

    sd

--- tute.java
+++ tute.java
@@ -6,3 +6,18 @@ class Tute{
         System.out.println("heloow");
     }
 }
+
+class keybord{
+    int keys = 10;
+    String ss = "keybord";
+    public void pressed() {
+        System.out.println("signal sent" + ss);
+    }
+
+    public void thssrow() {
+        System.out.println("got hit" + ss);
+        keys  =80;
+    }
+
+}
+//inheritence
